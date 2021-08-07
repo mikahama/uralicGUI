53c4972a89c94dc1f4c91bb64bda43223b2c4b72
Mika <mika.hamalainen@helsinki.fi>
2021-08-07 09:27:25 +0300

    download dialog, no scroll

--- uralicGUI/main_window.py
+++ uralicGUI/main_window.py
@@ -3,23 +3,29 @@ from kivy.uix.checkbox import CheckBox
 from kivy.uix.label import Label
 from kivy.uix.modalview import ModalView
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.recycleview import RecycleView
 
 from uralicNLP import uralicApi
 from uralicNLP import string_processing
+from kivy.core.window import Window
 
 from kivy.app import App
 
 class UralicApp(App):
 	pass
 
-def show_download():
-	view = ModalView(size_hint=(None, None), size=(400, 400))
-	layout = GridLayout(cols=1)
-	build_download_checkboxes(layout)
-	view.add_widget(layout)
+	def show_download(self):
+		view = ModalView(size_hint=(None, None), size=(400, 400))
+		layout = RecycleView(size_hint=(1, None), size=(400, 300))
+		g = GridLayout(cols=1)
+		build_download_checkboxes(g)
+		layout.add_widget(g)
+		view.add_widget(layout)
+		view.open()
 
 def build_download_checkboxes(layout):
 	langs = uralicApi.supported_languages()["morph"]
+	langs.sort()
 	for lang in langs:
 		installed = False
 		try:
@@ -27,7 +33,7 @@ def build_download_checkboxes(layout):
 			installed = True
 		except:
 			pass
-		l = GridLayout(cols=2)
-		l.add(CheckBox(active=installed))
-		l.add(Label(text=string_processing.iso_to_name(lang) + " - " +lang))
-		layout.add(l)
+		l = GridLayout(cols=2, size_hint_y=None, height=40)
+		l.add_widget(CheckBox(active=installed))
+		l.add_widget(Label(text=string_processing.iso_to_name(lang) + " - " +lang))
+		layout.add_widget(l)
