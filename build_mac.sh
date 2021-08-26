f1e0de5dc5d1c61ca79db2f6870da4a1fe665010
Mika Hämäläinen <mika@rootroo.com>
2021-08-26 21:21:42 +0300

    build script for mac

---
+++ build_mac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#python3 setup.py py2app
+#python3 -m PyInstaller --exclude-module enchant --exclude-module twisted --exclude-module _tkinter --exclude-module Tkinter --clean --windowed --name uralicNLP run.py
+
+python3 setup.py bdist_mac
+cp -R /Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages/kivy/* ./build/uralicNLP.app/Contents/MacOS/lib/kivy/
+codesign --remove-signature /Users/mikahama/uralicGUI/build/uralicNLP.app/Contents/MacOS/lib/Python
