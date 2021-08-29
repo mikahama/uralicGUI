7a01d1260ce7304ff2342b7904b982fc2ed77ae9
Mika Hämäläinen <mika@rootroo.com>
2021-08-29 15:36:20 +0300

    screenshots

--- build_mac.sh
+++ build_mac.sh
@@ -13,4 +13,5 @@ cp -r build/uralicNLP.app mac_dist_files/
 mv mac_dist_files/uralicNLP.app/Contents/MacOS/run mac_dist_files/uralicNLP.app/Contents/MacOS/run2
 cp mac_auxiliary_files/Launcher.app/Contents/MacOS/Launcher mac_dist_files/uralicNLP.app/Contents/MacOS/run
 cp -r mac_auxiliary_files/Launcher.app/Contents/Resources/* mac_dist_files/uralicNLP.app/Contents/Resources/
+rm -f mac_dist_files/uralicNLP.dmg
 appdmg mac_auxiliary_files/dmg.json mac_dist_files/uralicNLP.dmg
